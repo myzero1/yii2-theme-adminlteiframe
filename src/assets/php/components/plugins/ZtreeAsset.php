63f15df7a774563d7743d1ef9dc98cb2959bd75e
myzero1 <myzero1@sina.com>
2018-09-09 06:26:27 +0800

    ztree is ok now

--- src/assets/php/components/plugins/ZtreeAsset.php
+++ src/assets/php/components/plugins/ZtreeAsset.php
@@ -13,13 +13,13 @@ class ZtreeAsset extends AssetBundle
     public $sourcePath = '@vendor/myzero1/yii2-theme-adminlteiframe/src/assets/static/plugins';
     public $css = [
         'libs/ztree3/css/zTreeStyle/zTreeStyle.css',
+        // 'libs/ztree3/css/metroStyle/metroStyle.css',
     ];
 
     public $js = [
-        'libs/ztree3/js/jquery.ztree.core.js',
-        'libs/ztree3/js/jquery.ztree.excheck.js',
-        'libs/ztree3/js/jquery.ztree.excheck.js',
-        'libs/ztree3/js/jquery.ztree.exhide.js',
+        'libs/ztree3/js/jquery.ztree.core.min.js',
+        'libs/ztree3/js/jquery.ztree.excheck.min.js',
+        'libs/ztree3/js/jquery.ztree.exhide.min.js',
         'libs/ztree3/js/fuzzysearch.js',
         'use/ztree3.js',
     ];
