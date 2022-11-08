85d7afdf7a08f2f764d43322b266d34f996ecd8e
qinxuanwu <qinxuanwu@yungengxin.com>
2022-11-08 13:13:32 +0800

    formate

--- src/views/adminlteiframe/layouts/main.php
+++ src/views/adminlteiframe/layouts/main.php
@@ -14,37 +14,38 @@ myzero1\adminlteiframe\gii\GiiAsset::register($this);
 
 ?>
 
-<?php  $this->beginPage() ?>
+<?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html lang="<?=  Yii::$app->language ?>">
+<html lang="<?= Yii::$app->language ?>">
 
-    <head>
-        <meta charset="<?=  Yii::$app->charset ?>">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <?=  Html::csrfMetaTags() ?>
-        <title><?=  Html::encode($this->title) ?></title>
-        <!-- Tell the browser to be responsive to screen width -->
-        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
         <script src="../plugins/ie9/html5shiv.min.js"></script>
         <script src="../plugins/ie9/respond.min.js"></script>
         <![endif]-->
 
-        <?php  $this->head() ?>
-    </head>
-    <body class="iframe-list-content hold-transition skin-blue sidebar-mini" >
-        <?php  $this->beginBody() ?>
-        <!-- Main content -->
-        <section class="content list-content">
-            <?= myzero1\adminlteiframe\widgets\Alert::widget(); ?>
-            <?= $content ?>
-        </section>
-        <!-- /.content -->
-        <?php
-            $js = "
+    <?php $this->head() ?>
+</head>
+
+<body class="iframe-list-content hold-transition skin-blue sidebar-mini">
+    <?php $this->beginBody() ?>
+    <!-- Main content -->
+    <section class="content list-content">
+        <?= myzero1\adminlteiframe\widgets\Alert::widget(); ?>
+        <?= $content ?>
+    </section>
+    <!-- /.content -->
+    <?php
+    $js = "
                 if($('.adminlteiframe-alert').length > 0){
                     var t=setTimeout(function(){
                             clearTimeout(t);
@@ -58,9 +59,10 @@ myzero1\adminlteiframe\gii\GiiAsset::register($this);
                 }
             ";
 
-            $this->registerJs($js);
-        ?>
-        <?php  $this->endBody() ?>
-    </body>
+    $this->registerJs($js);
+    ?>
+    <?php $this->endBody() ?>
+</body>
+
 </html>
-<?php  $this->endPage() ?>
+<?php $this->endPage() ?>
