52ef39750d512185f4b764cfe91df83c8cb2650e
qinxuanwu <qinxuanwu@yungengxin.com>
2022-11-11 16:17:46 +0800

    formate

--- src/assets/static/plugins/use/table.js
+++ src/assets/static/plugins/use/table.js
@@ -1,14 +1,14 @@
-function applyz1table(){
-    $('table[data-provide="z1table"]').each(function() {
+function applyz1table() {
+    $('table[data-provide="z1table"]').each(function () {
         var defaultConfig = {
-          "fixedColumns" : true,
-          "height": 500,
-          "subtraction1" : 100,
-          "subtraction2Selector" : [],// ["#table1",".mytable","table"]
+            "fixedColumns": true,
+            "height": 500,
+            "subtraction1": 100,
+            "subtraction2Selector": [],// ["#table1",".mytable","table"]
         };
 
         var configSetting = $(this).attr('data-z1table-config');
-        if (configSetting=='undefied') {
+        if (configSetting == 'undefied') {
             configSetting = {};
         } else {
             configSetting = eval('(' + configSetting + ')');
@@ -19,7 +19,7 @@ function applyz1table(){
 
         fixTable(_that, config);
 
-        $(window).resize(_that, function(){
+        $(window).resize(_that, function () {
             fixTable(_that, config);
         });
 
@@ -27,16 +27,16 @@ function applyz1table(){
 }
 
 // applyz1table();
-setTimeout("applyz1table()",15);
+setTimeout("applyz1table()", 15);
 
 
-function getTableHeight(defaultConfig){
+function getTableHeight(defaultConfig) {
     var subtraction2 = 0;
     for (var i = defaultConfig.subtraction2Selector.length - 1; i >= 0; i--) {
-      if ($(defaultConfig.subtraction2Selector[i]).length > 0) {
-        // subtraction2 = subtraction2 + $(defaultConfig.subtraction2Selector[i]).height();
-        subtraction2 = subtraction2 + $(defaultConfig.subtraction2Selector[i]).outerHeight(true);
-      }
+        if ($(defaultConfig.subtraction2Selector[i]).length > 0) {
+            // subtraction2 = subtraction2 + $(defaultConfig.subtraction2Selector[i]).height();
+            subtraction2 = subtraction2 + $(defaultConfig.subtraction2Selector[i]).outerHeight(true);
+        }
     }
 
     subtractionNew = defaultConfig.subtraction1 + subtraction2;
@@ -45,15 +45,15 @@ function getTableHeight(defaultConfig){
     return heightNew;
 }
 
-function fixTable(target, defaultConfig){
+function fixTable(target, defaultConfig) {
     if (!(target.find(".empty").length > 0 || target.find("tbody").find("tr").length == 0)) {
-            if(typeof mybootstrapTable!="undefined"){
-                mybootstrapTable.bootstrapTable('destroy');
-            }
+        if (typeof mybootstrapTable != "undefined") {
+            mybootstrapTable.bootstrapTable('destroy');
+        }
 
-            mybootstrapTable = target.bootstrapTable('destroy').bootstrapTable({
-                height: getTableHeight(defaultConfig),
-                fixedColumns: defaultConfig.fixedColumns
-            });
+        mybootstrapTable = target.bootstrapTable('destroy').bootstrapTable({
+            height: getTableHeight(defaultConfig),
+            fixedColumns: defaultConfig.fixedColumns
+        });
     }
 }
